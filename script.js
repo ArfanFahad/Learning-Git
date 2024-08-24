b8229cbaff2aa1c74cce693c56366e325001393f
Md Fahad Farazi <arfanfahad40@gmail.com>
2024-08-24 21:04:23 +0600

    add some variable and trying to make a function which will make the background color change in one click and on the other click it will be in the normal state

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 
+const section_one = document.querySelector(".section_1"); 
+const section_two = document.querySelector(".section_2"); 
+
 
 
 
 const button_1 = document.querySelector(".btn_1"); 
+const button_2 = document.querySelector(".btn_2"); 
+
+
+
+
 
